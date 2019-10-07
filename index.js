3dbf1bc54f165044f6f041305bb7c30bfd196e7a
elijahmm17 <elijahmm17@gmail.com>
2019-10-07 13:41:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+function map(arr, fn) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i]));
+  }
+  return result;
+}
+
+function reduce(arr, fn, init) {
+  let result = init;
+  let i = 0;
+  
+  if(!init) {
+    result = arr[0];
+    i = 1;
+  }
+  
+  for (; i < arr.length; i++) {
+    result = fn(arr[i], result);
+  }
+  
+  return result;
+}
