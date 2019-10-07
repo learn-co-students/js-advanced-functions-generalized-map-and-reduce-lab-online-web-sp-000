321c6be4f80945f15c19745fd015320d6929c022
Andrew Davisson <adavisson@Andrews-MacBook-Air.local>
2019-10-07 10:14:53 -0600

    Done.

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ function map(array, func) {
   return newArray;
 }
 
-function reduce(array, func, sp = "0") {
-  let memo = sp;
-  for (let i = 0; i < array.length; i++) {
+function reduce(array, func, sp = 0) {
+  let memo;
+  let i;
+  if (!!sp) {
+    memo = sp;
+    i = 0;
+  } else {
+    memo = array[0];
+    i = 1;
+  }
+  while (i < array.length) {
     memo = func(array[i], memo);
+    i++;
   }
   return memo;
 }
