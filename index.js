5a82d52312cd0ff0330f2960ad25b1a6cec8fab0
Rachel <williamsrachelada@gmail.com>
2019-10-10 13:19:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(src, fn) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(fn(src[i]));
+  }
+  return r
+}
+
+function reduce(src, fn, startingValue) {
+  let total = startingValue || src[0];
+  let i = (!!startingValue) ? 0 : 1
+  for (; i < src.length; i++ ) {
+      total = fn(src[i], total)
+  }
+  return total;
+}
