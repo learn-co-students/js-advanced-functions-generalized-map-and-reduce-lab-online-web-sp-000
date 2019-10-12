b16aec7f18e2c6a159e9fb08119513021255985b
Shawn <shawntennity@gmail.com>
2019-10-12 17:36:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+function map(arr, func) {
+  const newArr = [];
+  arr.forEach(elem => newArr.push(func(elem)))
+  return newArr
+}
+
+function reduce(arr, func, startingPoint) {
+  if (startingPoint === undefined && func(arr[0], 0) !== false) {
+    startingPoint = 0
+    arr.forEach(elem => startingPoint = func(elem, startingPoint))
+    return startingPoint
+  } else if (startingPoint === undefined && func(arr[0], 0) === false) {
+    startingPoint = true
+    arr.forEach(elem => startingPoint = func(elem, startingPoint))
+    return startingPoint
+  } else {
+    arr.forEach(elem => startingPoint = func(elem, startingPoint))
+    return startingPoint
+  }
+}
+
+// sourceArray = [1, 2, true, "razmatazz"]
+// reduce(sourceArray, function(a, memo){ return !!a && !!memo})
