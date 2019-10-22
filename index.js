ae37dca27feb48dd4f69205545cd0a66895e7b6f
donna <donnacamos88@gmail.com>
2019-10-22 15:14:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
-function mapToNegativize(src) {
-    return map.r.push(-1 * src[i]) 
-}
+function map(src, cb) {
+    let r = []
+  
+    for (let i = 0; i < src.length; i++) {
+      let theElement = src[i]
+      r.push(cb(theElement))
+    }
+  
+    return r;
+  }
+  
+  function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
+
