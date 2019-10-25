13fb269f1dc201a942477e1c428b58ffed8ac324
abogailo <abagailanne@gmail.com>
2019-10-25 17:47:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your functions here
+function map(arr,func) {
+    return arr.map(element => func(element));
+}
+
+function reduce(arr, func, start) {
+    if (typeof start === "number") 
+       return arr.reduce((total, element) => func(total,element,start),start);
+    else 
+        return arr.reduce((total, element) => func(total,element,start));
+}
