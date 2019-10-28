a423a192347050a47c4e6829123199bbd7a39139
Tsunamins <reillyamr@gmail.com>
2019-10-28 15:01:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(array, applyFunction){
+    let newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(applyFunction(array[i]))
+    }
+    return newArray;
+}
+
+
+function reduce(array, applyFunction, startPoint){
+
+    let accumulatingVariable = 0;
+    if (startPoint !== undefined){
+        accumulatingVariable = startPoint;
+    } else {
+        accumulatingVariable = 0;
+    }
+    for(let i=0; i<array.length; i++){
+        accumulatingVariable = applyFunction(accumulatingVariable, array[i])
+    }
+    return accumulatingVariable;
+}  
+
