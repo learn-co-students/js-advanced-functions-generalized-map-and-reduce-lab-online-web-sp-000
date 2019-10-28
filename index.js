cdfa49009cbfc3c192787151757d02bf41fda109
ch2isk4kos <ch2isk4kos@gmail.com>
2019-10-28 18:52:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+
+function map(sourceArray, callBack) {
+   let newArray = [];
+
+   for (let i=0; i < sourceArray.length; i++) {
+      let element = sourceArray[i];
+
+      newArray.push(callBack(element));
+   }
+
+   return newArray;
+}
+
+function reduce(sourceArray, callBack, startingPoint) {
+   let newArray = (!!startingPoint) ? startingPoint : sourceArray[0];
+   let i = (!!startingPoint) ? 0 : 1;
+
+   for (; i < sourceArray.length; i++) {
+      newArray = callBack(sourceArray[i], newArray)
+   }
+
+   return newArray;
+}
