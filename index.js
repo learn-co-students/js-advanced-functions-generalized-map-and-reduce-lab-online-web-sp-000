3e97518bca7fc72a142825d191cfc27df1388c3d
Tsunamins <reillyamr@gmail.com>
2019-10-28 15:57:33 -0400

    Done.

--- index.js
+++ index.js
@@ -7,18 +7,38 @@ function map(array, applyFunction){
     return newArray;
 }
 
-
 function reduce(array, applyFunction, startPoint){
-
+    let i = 0;
     let accumulatingVariable = 0;
     if (startPoint !== undefined){
+        i = 0;
         accumulatingVariable = startPoint;
     } else {
-        accumulatingVariable = 0;
+        accumulatingVariable = array[0];
+        i = 1;
     }
-    for(let i=0; i<array.length; i++){
+    for(; i<array.length; i++){
         accumulatingVariable = applyFunction(accumulatingVariable, array[i])
     }
     return accumulatingVariable;
 }  
+
+
+
+
+
+
+// function reduce(array, applyFunction, startPoint){
+
+//     let accumulatingVariable = 0;
+//     if (startPoint !== undefined){
+//         accumulatingVariable = startPoint;
+//     } else {
+//         accumulatingVariable = 0;
+//     }
+//     for(let i=0; i<array.length; i++){
+//         accumulatingVariable = applyFunction(accumulatingVariable, array[i], i, array)
+//     }
+//     return accumulatingVariable;
+// }  
 
