352ec68d52e56a95996d118edfecf32750f6732a
davisjustinw <davisjustinw@gmail.com>
2019-11-07 20:52:31 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+let map = ( sourceArray, fun ) => {
+    let newArray = []
+    for ( let n of sourceArray ) {
+      newArray.push( fun( n ) )
+    }
+    return newArray
+  }
+
+  let reduce = ( sourceArray, fun, memo ) => {
+
+    for ( let n of sourceArray ) {
+      if(memo == undefined)
+        memo = n
+      else
+        memo = fun ( memo, n )
+    }
+    return memo
+  }
