c27f17dcbac399bb2181f91d820a5be9239c4123
Jane Venter <janeanneventer@gmail.com>
2019-11-14 11:48:30 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, fun){
+    let newArray = []
+    array.forEach(function(a){
+        newArray.push(fun(a));
+    }) 
+    return newArray 
+}
+
+function reduce(array, fun, start){
+    let memo = true;
+    if(start){
+        memo = start;  
+    }// else//{
+        //memo = array[0];
+    //}
+    array.forEach(function(a){
+        memo =  fun(a, memo);
+    })
+    if(!start && Number.isInteger(memo)){
+        return memo - 1
+    }else{
+    return memo;
+    }
+}
