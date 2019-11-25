5d928a70dd8e92a35966606d175bab9373c33eb3
jon <jonathan.wong110@gmail.com>
2019-11-25 11:43:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+
+// function map (array, method) {
+//     const array1 = array.map(x => method(x))
+//     return array1
+// }
+
+function map (array, method) {
+    const array1 = []
+    for (var i = 0; i < array.length; i++) {
+        array1.push(method(array[i]))
+    }
+    return array1
+}
+
+function reduce(array, method, startingPoint) {
+    let runningTotal
+    let i
+    if (startingPoint) {
+        runningTotal = startingPoint
+        i = 0
+    } else {
+        runningTotal = array[0]
+        i = 1
+    }
+    for (; i < array.length; i++) {
+        runningTotal = method(runningTotal, array[i])
+    }
+    return runningTotal
+}
