e669815533212da6c641dfeacb253ae46dd0ff03
Adil Naimi <adilnm23@hotmail.com>
2019-11-28 04:23:16 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+
+function map(arr,fn) {
+  let newArr=[]
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(fn(arr[i]))
+  }
+  return newArr
+}
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
