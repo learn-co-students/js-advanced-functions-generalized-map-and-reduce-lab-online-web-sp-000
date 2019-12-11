e33125b337c5ebfaf564980aa52c400e80f31fd4
Mark Daniel  Delgado <markpnoi26@gmail.com>
2019-12-11 21:48:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+function map(array, fn) {
+  let newArray = [];
+  array.forEach(element => {
+    newArray.push(fn(element));
+  });
+  return newArray;
+}
+
+function reduce(array, fn, starting = 0) {
+
+  // need value = true if no starting given and array NaN
+  // need value = 0 or starting if all array are numbers
+  
+  let value;
+  let arrayCondition = array.every(function(element) {return typeof element === 'number';})
+  
+  if (arrayCondition === true) {
+    value = starting;
+  } else {
+    value = true;
+  }
+  
+  array.forEach(element => {
+    value = fn(element, value)
+  })
+  return value
+}
+
