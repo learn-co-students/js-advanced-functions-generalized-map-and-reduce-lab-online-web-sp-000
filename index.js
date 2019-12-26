854a298a296938bfed0a9ace873c1ff983e53880
treuther <tracyreuther@gmail.com>
2019-12-26 13:11:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your functions here
+
+//write a generalized map and reduce function. Both of these functions will take an Array and a function
+//and require that you pass information between the function and the callback.
+
+//Your implementation should expect a source array and a function. All the tests will pass an Array and a block.
+//Remember, map returns a new Array.
 function map(src, cb) {
     let r = []
   
@@ -10,6 +16,13 @@ function map(src, cb) {
     return r;
 }
 
+//mapToSquare([1, 2, 3, -9]) //=> [1, 4, 9, 81]
+//became generalized as:
+//map([1, 2, 3, -9], function(a){ return a * a }) //=> [1, 4, 9, 81]
+
+
+//Your implementation should expect a source array and optionally a starting value. All the tests will
+//pass an Array and (sometimes) a starting point.
 function reduce(src, cb, starting){
     let r = (!!starting) ? starting : src[0]
     let i = (!!starting) ? 0 : 1
