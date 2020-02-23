3f13c63b09528d8f6acf0cf84ce7c5db3d1ac0bb
Michael <kofronmichael@gmail.com>
2020-02-23 11:39:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map(array, method){
+    let newArray = []
+
+    array.forEach(function(value){
+        newArray.push(method(value))
+    })
+
+    return newArray
+}
+
+function reduce(array, method, startingValue) {
+    let total = startingValue || array[0];
+    let i = (!!startingValue) ? 0 : 1
+    for (; i < array.length; i++ ) {
+        total = method(array[i], total)
+    }
+    return total;
+  }
+
