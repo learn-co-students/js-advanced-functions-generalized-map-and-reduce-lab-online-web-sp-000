c8b962775b96da08f8b48abd71526064f3a53650
tomciv9000 <eugene.white@gmail.com>
2020-02-24 00:34:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(srcArray, yourFunction){
+    let result = []
+    for (let i = 0; i < srcArray.length; i++) {
+        result.push(yourFunction(srcArray[i]))
+    }
+    return result
+}
+
+function reduce(srcArray, yourFunction, startingPoint){
+    let result = (!!startingPoint) ? startingPoint : srcArray[0]
+    let i = (!!startingPoint) ? 0 : 1
+    for ( ; i < srcArray.length; i++) {
+        result = yourFunction(srcArray[i], result)
+      }
+    
+    return result;
+}
