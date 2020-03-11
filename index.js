2eaceac880db2f40b64846b2ffd5ad0800eaee57
cperez1012 <perezchristian1012@gmail.com>
2020-03-11 13:40:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-function map(arr, fun) {
-    let newArray = [];
-     arr.forEach(e => {
-         newArray.push(fun(e))
-     });
-     return newArray
- }
-
-function reduce(arr, f, num = false) {
-    let returnVal = (num) ? num : arr[0];
-    let i = (num) ? 0 : 1;
- 
-    for (; i < arr.length; i++) {
-         returnVal = f(arr[i], returnVal)
+function map(src, cb) {
+    let r = []
+  
+    for (let i = 0; i < src.length; i++) {
+      let theElement = src[i]
+      r.push(cb(theElement))
     }
-    return returnVal
-}
+  
+    return r;
+  }
+  
+  function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
