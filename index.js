3a20e6ca3817ba6b442fe631aa08f670ac74ffaf
Amy Westcott <amy.westcott@yahoo.com>
2020-03-18 18:28:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your functions here
+function map(srcArr, fn) {
+    let newArr = []
+    for (let i = 0; i < srcArr.length; i++) {
+       const element = fn(srcArr[i])
+       newArr.push(element)
+    }
+    return newArr     
+ }
+
+
+
+
+function reduce(array, fn, startingPoint){
+    let i = 0
+    let accumulator = startingPoint
+
+    if (startingPoint === undefined) {
+        accumulator = array[0]
+        i = 1
+    }
+
+
+    for (;i < array.length; i++){
+        accumulator = fn(array[i], accumulator) 
+    }
+    return accumulator
+}
