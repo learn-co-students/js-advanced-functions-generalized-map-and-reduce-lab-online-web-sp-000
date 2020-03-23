1fbed428b45fd3a1b01195a7e3cd6a9d010baad8
rich0024 <yance0022@gmail.com>
2020-03-23 15:13:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, cb) {
+    const result = []
+    for (let x of array) {
+      result.push(cb(x))
+    }
+    return result
+}
+
+function reduce(array, cb, value) {
+  if (typeof value == "undefined") {
+    if (array.some(isNaN)) value = true
+    else value = 0
+  }
+  for (let x of array) {
+    value = cb(value,x,array)
+  }
+  return value
+}
