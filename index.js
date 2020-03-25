8ac04ecaf82502e129a9ba6129837b787b7f1ff3
BlythePollard <github email address>
2020-03-24 20:37:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(array, fn) {
+    let newArray = []
+    for(let i=0; i < array.length; i++) {
+        newArray.push(fn(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, fn, starting) {
+    // if(!!starting) {
+    //     let sum = starting
+    //     let i = 0
+    // } else { 
+    //     let sum = array[0]
+    //     let i = 1
+    // }
+    let r = (!!starting) ? starting : array[0]
+    let i = (!!starting) ? 0 : 1
+    
+    for (; i < array.length; i++) {
+       r = fn(r, array[i])
+    }
+    return r
+}
