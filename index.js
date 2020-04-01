d817316b39a1ad4e729420b8fb4f7ce7e16a6b4f
Lindsey Wells <github email address>
2020-04-01 10:37:10 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map(sourceArray, workFunction) {
+    let newArray = []
+
+    sourceArray.forEach(ting => {
+        newArray.push(workFunction(ting))
+    })
+    return newArray;
+}
+
+function reduce(sourceArray, workFunction, start = 0) {
+    let runningTotal = start
+    for (let element of sourceArray) {
+        runningTotal = workFunction(element, runningTotal)
+    }
+    return runningTotal;
+}
