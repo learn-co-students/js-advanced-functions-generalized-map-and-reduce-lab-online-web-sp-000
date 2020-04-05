133f9d87b2b8a4d3e0c3bc2c925348b5d133b7d1
Lauren McCoy <lm_lutec@outlook.com>
2020-04-05 10:45:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions here
+function map(arr, func){
+    let i;
+   let r = []
+
+   for (i = 0; i < arr.length; i++){
+       let element = arr[i]
+       r.push(func(element))
+   }
+   return r 
+}
+
+function reduce(arr, func, startPoint = 0){
+    let i;
+   
+    for (i = 0; i < arr.length; i++){
+        
+        if (startPoint){
+         i = 0
+        }
+        else {
+         i = 2
+         startPoint = arr[i]
+        }
+        while (i < arr.length) {
+            startPoint = func(startPoint, arr[i]);
+            i++;
+        }
+        return startPoint;
+   
+    }
+    
+     
+}
+
+
+
