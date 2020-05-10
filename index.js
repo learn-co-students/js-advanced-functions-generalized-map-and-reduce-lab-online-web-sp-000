74f1443802320bbbe8bd3a669eba3bd0bb07785f
github username <github email address>
2020-05-09 18:25:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+
+function map(sourceArray, funct){
+  let myArr = []
+  for (const i of sourceArray) {
+    myArr.push(funct(i));
+  }
+  return myArr;
+}
+
+function reduce(arr, funct, initialVal){
+  // handle either with initial value or not
+  let returnValue;
+  let i;
+  if (initialVal === undefined) {
+    returnValue = arr[0];
+    i=1;
+  } else {
+    returnValue = initialVal;
+    i=0;
+  }
+
+  for (;i< arr.length; i++){
+    returnValue = funct(returnValue, arr[i]);
+  }
+  return returnValue;
+}
