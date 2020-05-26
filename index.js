2d5775ff7f7b8e05a42ce0c1a3d142615c12b9d1
Christopher Dent <christopherdent01@gmail.com>
2020-05-26 18:30:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+// map method
+function map(array, callback) {
+const newArray = []
+for (let i = 0; i < array.length; i++) {
+  newArray.push(callback(array[i], i, this));
+}
+return newArray;
+}
+
+
+//reduce method
+function reduce(array, callback, start){
+    let r = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < array.length; i++) {
+      r = callback(array[i], r)
+    }
+  return r;
+  }
