e9330366110a479a7545067035f4e5c042be3f76
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-06-16 00:57:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(sourceArray, fn) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray = [...newArray, fn(sourceArray[i])];
+  }
+  return newArray;
+}
+
+function reduce(sourceArray, fn, startingPoint=0) {
+  let value;
+  let previous = sourceArray[0];
+  if (startingPoint !== 0) {
+    previous += startingPoint;
+  }
+  for(let i = 1; i < sourceArray.length; i++) {
+    value = fn(sourceArray[i], previous);
+    previous = value;
+  }
+  return value;
+}
