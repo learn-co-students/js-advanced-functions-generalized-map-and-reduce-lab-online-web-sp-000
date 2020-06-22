4d31488ee8a88de92f7636c6150b34a4dfd53dd0
nathan-stamand <nathansaint810@gmail.com>
2020-06-22 16:28:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(array, funct) {
+  let newArray = [];
+  for (const el of array) {
+    newArray.push(funct(el))
+  }
+  return newArray
+}
+
+function reduce(array, funct, startValue = "optional") {
+  if (startValue === "optional") {
+      return array.reduce(funct)
+  }
+  else {
+    return array.reduce(funct, startValue)
+  }
+}
