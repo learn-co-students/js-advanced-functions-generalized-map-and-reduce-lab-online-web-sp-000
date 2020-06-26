d4d129a28f7b9582b979e40d6281f1308edf0502
doakcovington <doakcovington@protonmail.com>
2020-06-26 13:56:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+const map = (arr, func) => {
+    let result  = [];
+    for (let i = 0; i < arr.length; i++){
+        let element = arr[i]
+        result.push(func(element))
+    }
+
+    return result;
+}
+
+const reduce = (arr, func, starting) => {
+    let result = (!!starting) ? starting : arr[0];
+    let i = (!!starting) ? 0 : 1;
+
+    for(; i < arr.length; i++){
+        result = func(arr[i], result)
+    }
+    return result;
+}
