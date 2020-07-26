eaf3cb0ea947de3d631fbf6873e6a9fac799f27f
github username <github email address>
2020-07-26 08:57:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+
+function map(sArray, array2) {
+    let newArray = []
+    for (let i = 0; i < sArray.length; i++) {
+      let element = sArray[i]
+      newArray.push(array2(element))
+    }
+    return newArray
+  }
+
+
+  function reduce(sArray, array2, starter) {
+    let x = (!!starter) ? starter : sArray[0]
+    let i = (!!starter) ? 0 : 1
+  
+    for (; i < sArray.length; i++) {
+      x = array2(sArray[i], x)
+    }
+  
+    return x;
+  }
