a6a030b2e5d7d0113728be4b3e94e9d7e6c1f42b
Alvin Coulson <aaacoulson@gmail.com>
2020-08-11 22:38:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+
+function map(arr, callbackFn) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        let e = arr[i];
+        newArr.push(callbackFn(e));
+    }
+    return newArr;
+}
+
+let testArr = [1,2,3];
+map(testArr, (e)=> { return e * e})
+
+
+function reduce(arr, callbackFn, initialValue){
+    let r = (!!initialValue) ? initialValue : arr[0]
+    let i = (!!initialValue) ? 0 : 1
+  
+    for (; i < arr.length; i++) {
+      r = callbackFn(arr[i], r)
+    }
+  
+    return r;
+}
