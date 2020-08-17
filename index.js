934c8b68396a4d7848095dff723f3c64ab71d8cc
bibraj96 <blendi.ibraj96@gmail.com>
2020-08-17 11:53:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(arr, callback) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let elm = arr[i];
+    newArr.push(callback(elm));
+  }
+  return newArr;
+}
+
+function reduce(srcArr, cb, starter) {
+  let x = (!!starter) ? starter : srcArr[0]
+  let i = (!!starter) ? 0 : 1
+
+  for (; i < srcArr.length; i++) {
+    x = cb(srcArr[i], x)
+  }
+
+  return x;
+}
