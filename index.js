8a2b7dce66dec6bb280b588dae9cfef1a7b21874
tibraj <tibraj94@gmail.com>
2020-08-31 11:57:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, callback) {
+    let arrayTwo = [];
+    for (let i = 0; i < array.length, i++) {
+        let element = array[i];
+        arrayTwo.push(callback(element));
+    }
+    return arrayTwo;
+}
+
+function reduce(array, callback, startingPoint) {
+    let notI = (!!startingPoint) ? startingPoint : array[0];
+    let i = (!!startingPoint) ? 0 : 1;
+    for (; i < array.length; i++) {
+        notI = callback(array[i], notI)
+    }
+    return notI;
+}
