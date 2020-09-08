fcae9fd205a6f9302eee93a13c130a4098231bb2
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-08 11:49:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+function map(array, block) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(block(array[i]));
+    }
+    return newArray;
+}
+
+function reduce(sourceArray, block, startingPoint) {
+    let memo;
+    let x;
+    if (startingPoint) {
+        memo = startingPoint;
+        x = 0;
+    } else {
+        memo = sourceArray[0];
+        x = 1;
+    } 
+    for (let i = x; i < sourceArray.length; i++) {
+        memo = block(sourceArray[i], memo);
+    }
+    return memo;
+}
