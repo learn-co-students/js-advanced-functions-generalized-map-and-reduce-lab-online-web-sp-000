6cf26645e7e5fa1a6862dc828201af19ddcf46c9
github username <github email address>
2020-09-19 19:45:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+//callback: passed function (since the receiving function will execute or call that function "call it back")
+//function expression: anonymous function
+//pointer: variable name, declared function name
+
+//pass callback to a function or within a function
+//arity: "how many arguments are expected"
+//map and reduce has a lot of repeated code 
+//call the function by typing its pointer name and adding ()
+
+
+//map returns a new array 
+function map(array, func){ //take an array and a function
+    let r = []
+    for (let i = 0; i < array.length; i++){
+      r[i] = func(array[i])
+    }
+    return r
+  }
+  
+  //reduce returns a value 
+  function reduce(array, func, startingPoint){
+    let r = startingPoint || 0
+    for(let i = 0; i < array.length; i++){
+      r = func(array[i], r)
+    }
+    return r
+  }
