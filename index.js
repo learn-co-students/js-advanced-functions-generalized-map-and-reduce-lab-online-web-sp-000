16161f273afdc202b7adf0e682a4ec9e2f145361
Scott Hoisington <hoyzington@gmail.com>
2020-09-24 04:42:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(array, func) {
+  let result = [];
+  for (const item of array) {
+    result.push(func(item));
+  }
+  return result;
+}
+
+function reduce(source, func, start){
+  let result = !!start ? start : source[0];
+  let array = !!start ? source : source.slice(1);
+  for (const item of array) {
+    result = func(item, result);
+  }
+  return result;
+}
