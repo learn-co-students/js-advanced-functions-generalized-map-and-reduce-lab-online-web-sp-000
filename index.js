d541471d1524b32c9dd5baad6cafa588903125a0
ting682 <tchung682@gmail.com>
2020-09-25 08:56:06 -0400

    Done.

--- index.js
+++ index.js
@@ -10,19 +10,14 @@ function map(array, func) {
 
 function reduce(array, func, startingPoint) {
     
-    //debugger
+    
     let agg
     if (startingPoint) {
         agg = startingPoint
      }  
-    //else if (array[0]) {
-    //     agg = 0
-    // } else {
-    //     agg = 0
-    // }
 
     for (let i = 0; i < array.length; i++) {
-        debugger
+        
         if (typeof agg === "undefined") {
             agg = func(array[1], array[0])
             i++
