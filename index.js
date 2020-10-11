c172a1e7020f9e87a448e6bb6757e0794d2de3ea
Emily Gann <aspentree79@gmail.com>
2020-10-11 14:42:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(src, callback) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        let var1 = src[i]
+        r.push(callback(var1))
+    }
+    return r;
+}
+
+function reduce(src, callback, start) {
+    let r = (!!start) ? start : src[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < src.length; i++) {
+        r = callback(src[i], r)
+    }
+    return  r;
+}
