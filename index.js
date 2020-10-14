3cfff1929c25954c32b9b254d61a0c6294e39c63
Indre Bakeer <indre.bakeer@gmail.com>
2020-10-14 17:00:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(sourceArray, callback) {
+  let array = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    let value = sourceArray[i]
+    array.push(callback(value))
+  }
+  return array;
+}
+
+function reduce(sourceArray, callback, startingPoint) {
+  let total = (!!startingPoint) ? startingPoint : sourceArray[0]
+  let i = (!!startingPoint) ? 0 : 1
+
+  for (; i < sourceArray.length; i++) {
+    total = callback(sourceArray[i], total)
+  }
+  return total;
+}
