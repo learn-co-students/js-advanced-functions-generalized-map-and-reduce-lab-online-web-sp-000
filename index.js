e23cd2fff4686fd129dc000e818811befa4e66ab
carrollm2 <carrollm2@gmail.com>
2020-10-16 01:20:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your functions here
+
+function map(sourceArray, callbackFunction){
+
+    const mappedArray = [];
+    for (let i = 0; i < sourceArray.length; i++){
+        mappedArray.push(callbackFunction(sourceArray[i]));
+    }
+    return mappedArray;
+}
+
+
+
+function reduce(sourceArray, callbackFunction, startingPoint=0) {
+
+    if (startingPoint && typeof startingPoint !== "boolean"){
+        var reducedValue = startingPoint
+    } else if (startingPoint === 0) {
+        var reducedValue = 0;
+    } else {
+        var reducedValue = true;
+    }
+
+    for (let i = 0; i < sourceArray.length; i++) {
+        reducedValue = callbackFunction(reducedValue, sourceArray[i]);
+    }
+
+    return reducedValue;
+
+} 
+
+
