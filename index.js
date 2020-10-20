af2ae54895fe992ef753600fecf492289cb5cf1f
jessicagomesng <jgnpeluso@gmail.com>
2020-10-20 16:52:58 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+// function map(src, fn) {
+//     return src.map(x => fn(x));
+// }
+
+function map(src, fn) {
+    let r = []
+
+    for (let i = 0; i < src.length; i++) {
+        r.push(fn(src[i]));
+    }
+
+    return r;
+}
+
+// function reduce(src, fn, startingValue = 0) {
+//     return src.reduce(fn, startingValue);
+// }
+
+function reduce(src, fn, starting = 0) {
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < src.length; i++ ) {
+        r = fn(src[i], r)
+    }
+
+    return r;
+}
