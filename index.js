8725d13977fa19f6bce6d80ba2fc344e25cb5b68
rohanphillips <rohan@solutions-insurance.com>
2020-10-26 20:02:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+function map(sourceArray, block){
+  for (let i=0; i < sourceArray.length; i++){
+    sourceArray[i] = block(sourceArray[i]);
+  }
+  return sourceArray;
+}
+
+function reduce(sourceArray, block, startingPoint){
+  let myTotal = 0;
+  if (startingPoint){
+    myTotal = startingPoint;
+  }
+  for (let i=0; i < sourceArray.length; i++){
+    myTotal = block(myTotal, sourceArray[i]);
+  }
+  return myTotal;
+}
+const fn = function(a, memo){ return !!a && !!memo};
+//sourceArray = [1, 2, true, "razmatazz", false];
+sourceArray = [1, 2, true, "razmatazz"];
