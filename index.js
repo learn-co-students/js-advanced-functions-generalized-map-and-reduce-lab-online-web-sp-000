a4e6994273df4d331bada075b36b6ce637a78652
Majick333 <rmccauleycode@gmail.com>
2020-10-26 20:47:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, change) {
+    let x = []
+    for(let i = 0; i < array.length; i++) {
+        let base = array[i]
+        x.push(change(base))
+    }
+    return x
+}
+
+function reduce(array, change, optVal) {
+    let x = (!!optVal) ? optVal : array[0]
+    let i = (!!optVal) ? 0 : 1
+    for (; i < array.length; i++) {
+        x = change(array[i], x)
+      }
+    
+      return x;
+}
+
