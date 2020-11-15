c57f305a9acb8e6a0d8c0092e579a0831479b5ef
ShaylaWhite <shayladwhite1@gmail.com>
2020-11-14 16:15:36 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+// Add your functions 
+
+function map(source, callback) {
+    let r = []
+  
+    for (let i = 0; i < source.length; i++) {
+      let theElement = source[i]
+      r.push(callback(theElement))
+    }
+  
+    return r;
+  }
+  
+  function reduce(source, callback, starting){
+    let r = (!!starting) ? starting : source[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < source.length; i++) {
+      r = callback(source[i], r)
+    }
+  
+    return r;
+  }
