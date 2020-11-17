94b32e0103674c2f506f0e70f5d5141b8dadc275
blumenthaler <blumenthaleralex@gmail.com>
2020-11-17 13:22:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your functions here
+function map(array, fn) {
+    return array.map(value => fn(value))
+}
+
+function reduce(array, fn, start=0) {
+    let memo = (!!start) ? start : array[0];
+    let i = (!!start) ? 0 : 1;
+
+    for (i; i < array.length; i++) {
+        memo = fn(array[i], memo)
+    }
+    return memo
+}
