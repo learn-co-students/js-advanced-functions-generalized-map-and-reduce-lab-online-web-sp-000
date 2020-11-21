ca56ff483b6248d51f45a0127455cb65ca834f45
The-Gentleman <zraja4402@gmail.com>
2020-11-21 11:41:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, operation){
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+        newArray.push(operation(array[i]));
+    }
+    return newArray;
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
