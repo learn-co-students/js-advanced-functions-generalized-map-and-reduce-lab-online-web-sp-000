280d4f5c2db3fc2a533523b2bd4ae547c9a82e45
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-10 23:23:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+ function map(source, callback) {
+   let r = []
+   for (let i = 0; i < source.length; i ++) {
+     let el = source[i]
+     r.push(callback(el))
+   }
+   return r
+ }
+
+function reduce(source, callback, sv=0) {
+  let r = (!!sv) ? sv : source[0]
+  let i = (!!sv) ? 0 : 1
+  for (;i < source.length; i++) {
+    r = callback(source[i], r)
+  }
+  return r
+}
