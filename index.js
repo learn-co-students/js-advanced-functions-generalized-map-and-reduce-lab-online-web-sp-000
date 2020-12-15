64379c2164eec2790c5f38f2e7f006a22429db19
mlgvla <mlgerardvla@gmail.com>
2020-12-14 21:31:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+const map = function(sourceArray, callback){
+    let newArray = []
+
+    for (let index = 0; index < sourceArray.length; index++) {
+        newArray.push(callback(sourceArray[index]))
+    }
+    return newArray
+}
+
+const reduce = function(sourceArray, callback, startingValue) {
+    let memo = startingValue ? startingValue : sourceArray[0]
+
+    for (let index = startingValue ? 0 : 1; index < sourceArray.length; index++) {
+        memo = callback(sourceArray[index], memo)   
+    }
+    return memo
+}
