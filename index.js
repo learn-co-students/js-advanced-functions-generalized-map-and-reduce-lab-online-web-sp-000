1011a0106d7cb4421a0cb33c9468885319d649af
Joseph <DeGregorio.Joseph@gmail.com>
2020-12-19 16:36:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, block){
+    let newArray = [];
+    for (let i = 0; i< array.length; i++){
+        newArray.push(block(array[i]));
+    };
+    return newArray;
+}
+
+function reduce(array, block, start){
+    let v= (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+    for (; i< array.length; i++){
+        v = block(array[i], v)
+    };
+    return v;
+}
