270009c4b93f6f1c5f9fee5d79065f53b3449cff
CoderJay06 <jaycru06@yahoo.com>
2020-12-20 05:21:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+const map = (sourceArray, callback) => {
+    const newArray = []
+    sourceArray.forEach(value => {
+        newArray.push(callback(value))
+    });
+    return newArray
+}
+
+const reduce = (sourceArray, callback, startingVal = 0) => {
+    let result
+    let total = 0
+    sourceArray.forEach(value => {
+        total += value
+        if (!value) {
+            total = false
+        } else {
+            total = true
+        }
+    })
+    result = callback(total, startingVal)
+
+    return result
+}
