c155e437512cb1604be1ad217be68794452e71e4
Joseph Perez <hello@josephdanielperez.com>
2020-12-19 20:00:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(src, func) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        let element = src[i]
+        r.push(func(element))
+    }
+
+    return r;
+}
+
+function reduce(src, func, starting) {
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < src.length; i++) {
+        r = func(src[i], r)
+    }
+
+    return r;
+}
