af722524cb7c491958e367996f17322f2cf89e72
Yumacorn <ReachRyanMui@gmail.com>
2021-01-04 00:21:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(set, arg) {
+    let result = [];
+    for (let n of set) {
+        result.push(arg(n));
+    }
+    return result;
+}
+
+
+function reduce(set, arg, result = 0) {
+    // let result = 0;
+    for (let n of set) {
+        result = arg(n, result);
+    }
+    return result;
+}
