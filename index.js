b2835288645ec23d051128be55df2da8bb45e5ec
SVRourke <samuel@rourke.tech>
2021-01-12 20:48:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(source, callback) {
+    let result = [];
+    for (const value of source) {result.push(callback(value))}
+    return result
+}
+
+function reduce(source, callback, startingValue) {
+    let result = (!!startingValue) ? startingValue : source[0];
+    let i = (!!startingValue) ? 0 : 1;
+
+    for (;i < source.length; i++) {
+        result = callback(source[i], result)
+    }
+    return result
+}
