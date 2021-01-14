476186ae7f3bad7e4319139e297027c482866bf1
Drew Thompson <drewallynthompson@gmail.com>
2021-01-14 08:44:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map (obj, callback){
+  let newArr = [];
+  for (const key in obj){
+    newArr.push(callback(obj[key]))
+  }
+  return newArr;
+}
+
+function reduce (obj, callback, accum){
+  let i = 1;
+  accum? i = 0 : accum = obj[0];
+  for (i; i < obj.length; i++){
+    accum = callback(accum, obj[i]);
+  }
+  return accum;
+}
