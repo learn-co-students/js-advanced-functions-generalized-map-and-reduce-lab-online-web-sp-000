fb7d206b4b14e5ae4453340bf90740fca69e40af
Brian Vehige <brianvehige@gmail.com>
2021-01-22 18:03:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map (arr, func){
+    const result = []
+    for (let element of arr) {
+        result.push(func(element))
+    }
+    return result
+}
+
+function reduce (arr, func, start){
+    if (!!start===false){
+        start = arr.shift()
+    }
+    return arr.reduce(func, start)
+}
