1c10ce6f60fa29b6c6e8e4e04c6c67a0768d877d
David Strauch <dmaster18@gmail.com>
2021-01-24 04:32:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-function map(src, fn) {
-  let r = [];
-  for (let i = 0; i < src.length; i++) {
-    r.push(fn(src[i]));
-  }
-  return r;
+// Add your functions here
+function map (arr, func){
+    const result = []
+    for (let element of arr) {
+        result.push(func(element))
+    }
+    return result
 }
 
-function reduce(src, fn, startingPoint = 0) {
-  let sum = startingPoint;
-  for (let i = 0; i < src.length; i++) {
-    if (startingPoint !== 0) {
-       sum = sum + fn(src[i]);
-     } else {
-        sum = fn(src[i]);
-     }
-  }
-  return sum;
+function reduce (arr, func, start){
+    if (!!start===false){
+        start = arr.shift()
+    }
+    return arr.reduce(func, start)
 }
