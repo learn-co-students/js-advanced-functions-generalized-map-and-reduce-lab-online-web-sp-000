fa6b4fbd568d3256eea45770e877d27be447dc5e
seth <seth.j.near@gmail.com>
2021-01-24 17:26:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+
+
+function map(sourceArray, fn) {
+    let result = []
+    for (const e of sourceArray) {
+        result.push(fn(e));
+    }
+    return result;
+}
+
+function reduce(sourceArray, fn, startingValue) {
+
+let result = !!startingValue ? startingValue : sourceArray[0]
+let i = !!startingValue ? 0 : 1
+
+for (; i < sourceArray.length; i++) {
+
+    const e = sourceArray[i];
+    result = fn(e, result)
+    
+}
+return result;
+}
