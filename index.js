e008d7b9c94befc50876f5f531aa167021974b5e
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-26 15:32:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(array, callback) {
+  let temp = [];
+
+  for (let i = 0; i < array.length; i++) {
+    temp.push(callback(array[i]));
+  }
+
+  return temp;
+}
+
+function reduce(array, callback, start = 0) {
+  let temp = !!start ? start : array[0];
+  let i = !!start ? 0 : 1;
+
+  for (; i < array.length; i++) {
+    temp = callback(array[i], temp);
+  }
+
+  return temp;
+}
