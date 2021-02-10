0396143fd9a96947f0355141e6fa05ae1e9d105d
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-02-09 20:59:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+ function map(src, fn){
+    let newSrc = []
+     for (let i = 0; i < src.length; i++){
+         newSrc.push(fn(src[i]))
+     }
+     return newSrc
+ }
+
+function reduce(src, fn, start = 0){
+    let finalValue = (!!start) ? start : src[0]
+    for (let i = (!!start) ? 0 : 1; i < src.length; i++){
+        finalValue = fn(src[i], finalValue)
+    }
+    return finalValue
+}
