0c27d8910c0c5148adfd60b94df4cc83e32a627e
anpham148 <anpham148@gmail.com>
2021-02-12 13:40:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+// Add your functions 
+
+
+function map(arr, func) {
+  let r = [];
+  for (let i = 0; i < arr.length; i++) {
+    r.push(func(arr[i]));
+};
+  return r
+}
+
+ function reduce(arr, func, startingPoint = 0) {
+     let r = startingPoint ? startingPoint : arr[0];
+     let i = startingPoint ? 0 : 1;
+     for (; i < arr.length; i++) {
+      r = func(arr[i], r)
+      
+  };
+  
+  return r
+ }
+
+
+
+
