6e68532b8574a6fc3e8290349c3fa84f88396661
khoinguyenkc <khoinguyenkc@gmail.com>
2021-02-15 09:58:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your functions here
+
+function map(sourceArray, callbackFunction) {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        newArray.push(callbackFunction(sourceArray[i])) 
+    }
+    return newArray
+  
+};
+
+
+// function reduce(sourceArray, callbackFunction, startingPoint = 0 )  {
+//     let accumulator = 0;
+//     if (startingPoint) {
+//          accumulator = startingPoint;
+//     }
+
+//     for (let i = 0; i < sourceArray.length; i++ ) {
+//         accumulator = callbackFunction(sourceArray[i], accumulator);
+//     }
+//     return accumulator;
+  
+// };
+
+function reduce(array, callback, startingValue){
+    let i;
+    let acc;
+    if (startingValue) {
+        acc = startingValue;
+        i = 0;
+    } else {
+        acc = array[0];
+        i = 1;
+    };
+
+    for (; i < array.length; i ++ ) {
+        //notice the ; at the start. its weird like that!!!!
+        acc = callback(acc, array[i]);
+    };
+
+    return acc;
+
+
+
+
+  }
+  
