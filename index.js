059949ffb2a6a9ad917af42951e7026cc7825afd
AmyFong82 <amyhfong@gmail.com>
2021-02-17 02:03:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, fn){
+	let newArray = []
+  	for (let i = 0; i < array.length; i++ ) {
+		newArray.push(fn(array[i]))
+	}
+	return newArray
+}
+
+function reduce(array, fn, startingPt=0){
+	let value
+	if (typeof startingPt === 'number') { value = startingPt}
+		let valArray = []
+	for (let i = 0; i < array.length; i++){
+		value = fn(array[i], value)
+	}
+
+	return value
+}
