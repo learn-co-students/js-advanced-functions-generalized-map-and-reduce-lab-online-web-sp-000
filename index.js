7a1581afbf13b906fea2169dff10d34da22f942d
jrog20 <jaimerogstad@gmail.com>
2021-02-16 21:31:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your functions here
+function map(src, block) {
+  return src.map(block);
+}
+
+function reduce(src, block, starting=0) {
+  if(!!starting) {
+    return src.reduce(block, starting);
+  }
+  else {
+    return src.reduce(block);
+  }
+}
