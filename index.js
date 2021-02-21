19b05d80ac1bbfb5ee144637f411b03657c5caa8
Jake <jake098877@gmail.com>
2021-02-21 12:14:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(arr, fn) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(fn(arr[i]));
+  }
+  return newArr;
+}
+
+function reduce(arr, fn, startingPoint=0) {
+  let sum = startingPoint;
+  for (let i = 0; i < arr.length; i++) {
+    sum = fn(sum, arr[i]);
+  }
+  return sum;
+}
