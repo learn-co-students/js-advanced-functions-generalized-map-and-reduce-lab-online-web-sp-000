99c4389724223bdc3d4686b72ca840ea9e96fd51
Chandra Lama <iamme24cl@gmail.com>
2021-03-05 14:07:45 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map(arr, fn) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(fn(arr[i]));
+  }
+  return newArr;
+}
+
+function reduce(arr, fn, startPoint) {
+  let reducedValue = startPoint || arr[0];
+  let i = (!!startPoint) ? 0 : 1;
+
+  for (; i < arr.length; i++) {
+   reducedValue = fn(arr[i], reducedValue)
+  }
+  return reducedValue;
+}
+
+
