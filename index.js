8a9e31104cd01fceceaf0857d0502a3fb8cf64af
yarvinh <yarvinhhernandez>
2021-03-14 17:18:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your functions here
+function map(values,func){
+  return values.map(function(v){
+     return func(v)
+  })
+}
+
+function reduce(values,func,startingPoint = 0){
+    let value = []
+
+    values.map(function(v){
+        if(typeof v === 'number'){
+          startingPoint = func(v,startingPoint)
+        } else {
+          value.push(func(v,value))
+        }
+    })
+
+    if (value.includes(false)){
+        return false
+    } else if (!value.includes(false)){
+        if(startingPoint > 0){
+            return startingPoint
+        } else {
+             return true
+        }
+       
+    }
+
+}
+     
+
+
