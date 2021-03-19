96d86e6cb14b62af60cfab986eef1de1ed34026b
Carrie Mathieu <carrieburks@gmail.com>
2021-03-19 18:19:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+// map handles loops, functions = mapToDouble, mapToSquare, etc
+function map(sourceArray, func) {
+    let returnValue = sourceArray.map(func)
+    return returnValue
+}
+
+function reduce(sourceArray, func, startingPoint=0) {
+    let returnValue;
+
+    // if there's a starting point, execute the following block, passing in the function & starting point as arguments
+    if (!!startingPoint) { 
+        returnValue = sourceArray.reduce(func, startingPoint)
+    } else { // else, execute following block, only passing in function as argument
+        returnValue = sourceArray.reduce(func)
+    }
+    return returnValue 
+}
