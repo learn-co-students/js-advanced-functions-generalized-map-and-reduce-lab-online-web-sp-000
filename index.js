9b863b8a088befab6317e5d65db91604204d3440
Arodriguez1030 <arodriguez1030@gmail.com>
2021-03-26 11:19:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, callb) {
+    let r =[]
+    for (let i =0; i < arr.length; i++){
+        let theIndex = arr[i]
+        r.push(callb(theIndex));
+    }
+    return r
+}
+
+function reduce(arr, callb, starting) {
+    let r = (!!starting) ? starting : arr[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < arr.length; i++) {
+        r = callb(arr[i], r)
+    }
+    return r
+}
