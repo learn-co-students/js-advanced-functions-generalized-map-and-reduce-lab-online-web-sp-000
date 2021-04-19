30da3af18852430b567dc35c531a2db8249439fc
nlanese21 <nlanese21@gmail.com>
2021-04-18 21:45:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+function map(arr, func){
+    return arr.map(x => func(x))
+}
+
+function reduce(arr, func, start){
+    if (start > 0 || start < 100){
+       return arr.reduce(func, start)
+    }
+    else{
+        return arr.reduce(func, 0)
+    }
+}
