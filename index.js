4bbb6f30e0c3de12436ea73429f82708e0f98983
Alison Williams <apw5011@gmail.com>
2021-04-21 18:26:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, f) {
+    let newArray = [] 
+    for (let i=0; i<array.length; i++) {
+        let e = array[i]
+        newArray.push(f(e))
+    }
+    return newArray
+}
+
+function reduce(array, f, sp=0) {
+    let value = (!!sp) ? sp : array[0]
+    let i = (!!sp) ? 0 : 1
+
+    for (; i<array.length; i++) {
+        value = f(array[i], value)
+    }
+    return value;
+}
+
