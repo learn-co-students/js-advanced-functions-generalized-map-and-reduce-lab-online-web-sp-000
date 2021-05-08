e8512f26fd142834f13a0e976741f3d5f544db23
msteele96 <marcsteele2014@gmail.com>
2021-05-08 09:29:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map(array, func) {
+    const newArray = []
+    array.forEach(element => {
+        newArray.push(func(element))
+    });
+
+    return newArray
+}
+
+function reduce(array, func, start) {
+    let current = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < array.length; i++) {
+      current = func(array[i], current)
+    }
+  
+    return current;
+}
