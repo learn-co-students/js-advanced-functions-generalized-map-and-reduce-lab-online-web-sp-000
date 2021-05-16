394cd0701feb34a215ed24273d1898d7e0ce2ef8
Ryan Burns <ryankburns@gmail.com>
2021-05-16 10:12:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+
+function map(sourceArray, callback) {
+    const newArr = sourceArray.map(x => callback(x))
+    return newArr
+}
+
+function reduce(sourceArray, callback, startingPoint) {
+    if (startingPoint) {
+        const newTotal = sourceArray.reduce((acc, val) => callback(acc, val), startingPoint)
+        return newTotal
+    }
+    else {
+        const newTotal = sourceArray.reduce((acc, val) => callback(acc, val))
+        return newTotal
+    }
+}
