9f78b10e6294a1d540708424844b765ddb00860e
bongofury <jtroutmanman@gmail.com>
2021-05-23 20:57:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(startingArray, callback) {
+    let result = []
+    startingArray.forEach(element => {
+        result.push(callback(element));
+    });
+    return result;
+}
+
+function reduce(startingArray, callback, startingPoint) {
+    let memo = startingArray[0]
+    for (let i = 1; i < startingArray.length; i++) {
+       memo = callback(startingArray[i], memo);
+    }
+    if (startingPoint) {
+        return startingPoint + memo;
+    } else {
+        return memo;
+    }
+}
